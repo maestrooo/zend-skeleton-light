2c7e89a50126a8ffa15d0da44d6d6c5d79a8354a
Michaël Gallego <mic.gallego@gmail.com>
2013-05-27 12:09:46 +0300

    Add one more error to report (useful when developing)

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 // This makes our life easier when dealing with paths. Everything is relative to the application root now
 chdir(dirname(__DIR__));
 
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_RECOVERABLE_ERROR);
 
 // Composer autoloading
 if (file_exists('vendor/autoload.php')) {
